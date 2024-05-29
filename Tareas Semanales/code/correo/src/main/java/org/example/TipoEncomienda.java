833812ddab8f6d34f6f83445a8604bee3e30b4b7
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-05-29 17:38:55 -0300

    Reorganizo

--- Tareas Semanales/code/correo/src/main/java/org/example/TipoEncomienda.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum TipoEncomienda {
-    HECHA_POR_PERSONA,
-    HECHA_POR_CORREO
-}
--- Tareas Semanales/code/correo/src/main/java/org/example/TipoTelegrama.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public enum TipoTelegrama {
-    CARTA_DOCUMENTO,
-    TELEGRAMA_POR_ENVIO,
-    INVITACION
-}
