5c776b1a39f3c61e2f1f5eec0d19eb35b5631c6c
Adrian Gil <agiltolentino@frba.utn.edu.ar>
2024-06-08 18:45:14 -0300

    Api Mock

---
+++ src/main/java/apiMock/dtos/PuntosEstrategicos.java
@@ -0,0 +1,15 @@
+package apiMock.dtos;
+
+import java.util.List;
+
+public class PuntosEstrategicos {
+    private List<PuntoEstrategico> puntos;
+
+    public List<PuntoEstrategico> getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(List<PuntoEstrategico> puntos) {
+        this.puntos = puntos;
+    }
+}
