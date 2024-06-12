464c84d7eda2a06ff4b7be97f68b498fa4a4781f
Adrian Gil <agiltolentino@frba.utn.edu.ar>
2024-06-12 17:25:38 -0300

    Api mock

--- src/main/java/apiMock/ApiMockCall.java
+++ src/main/java/apiMock/ApiMockCall.java
@@ -1,7 +1,6 @@
 package apiMock;
 
 import apiMock.dtos.PuntoEstrategico;
-import apiMock.dtos.PuntosEstrategicos;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,11 +8,8 @@ import org.apache.cxf.jaxrs.client.WebClient;
 
 import javax.ws.rs.core.Response;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ApiMockCall {
-    public List<PuntoEstrategico> punto_estrategico(Double latitud, Double longitud, Double radio) throws Exception {
+    public PuntoEstrategico[] punto_estrategico(Double latitud, Double longitud, Double radio) throws Exception {
         WebClient clientUsers = WebClient.create("https://05fdedf2-260a-4266-a358-c85cb1f7e468.mock.pstmn.io/api/puntosEstrategicos");
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -25,19 +21,9 @@ public class ApiMockCall {
         String responseBody = response.readEntity(String.class);
         if (status == 200) {
 
-            PuntosEstrategicos puntosEstrategicos = objectMapper.readValue(responseBody, PuntosEstrategicos.class);
+            PuntoEstrategico[] puntosEstrategicos = objectMapper.readValue(responseBody, PuntoEstrategico[].class);
 
-            List<PuntoEstrategico> puntos = new ArrayList<>();
-
-
-            for (int i=0; i<puntosEstrategicos.getPuntos().size(); i++) {
-                PuntoEstrategico punto = puntosEstrategicos.getPuntos().get(i);
-
-                puntos.add(punto);
-
-            }
-
-            return puntos;
+            return puntosEstrategicos;
 
 
         } else {
