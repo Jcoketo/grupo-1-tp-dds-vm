9d0fec7b45642e988f0e9c52e22621fc30a857d0
smarquesgomez <smarquesgomez@frba.utn.edu.ar>
2024-07-07 21:07:39 -0300

    subocambios

--- src/main/java/personas/PersonaVulnerable.java
+++ src/main/java/personas/PersonaVulnerable.java
@@ -5,12 +5,9 @@ import java.time.LocalDate;
 import elementos.TarjetaPlastica;
 
 public class PersonaVulnerable {
-    private String nombre;
-    private LocalDate fechaNacimiento;
     private LocalDate fechaRegistro;
-    private String domicilio;
-    private Documento documento;
     private int menoresACargo;
     private TarjetaPlastica tarjeta;
     private PersonaHumana dioAlta;
+    private PersonaHumana persona;
 }
--- src/main/java/personas/Tecnico.java
+++ src/main/java/personas/Tecnico.java
@@ -1,10 +1,7 @@
 package personas;
 
 public class Tecnico {
-    private String nombre;
-    private String apellido;
-    private Documento documento;
     private String nroCUIL;
-    private MedioDeContacto medioContacto;
     private String areaCobertura;
+    private PersonaHumana persona;
 }
