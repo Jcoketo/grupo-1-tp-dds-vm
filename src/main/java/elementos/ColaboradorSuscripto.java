7a929ad9df369208529cf3f24114797add012ea2
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-07-27 21:40:54 -0300

    push

--- src/main/java/elementos/ColaboradorSuscripto.java
+++ src/main/java/elementos/ColaboradorSuscripto.java
@@ -2,14 +2,19 @@ package elementos;
 
 import personas.Colaborador;
 
-public class ColaboradorSuscripto {
+public abstract class ColaboradorSuscripto {
     private Heladera heladera;
     private Colaborador colaborador;
     private TipoSuscripcion tipo;
+    private Integer limiteViandasMinimas;
+    private Integer limiteViandasMaximas;
 
     public ColaboradorSuscripto(Heladera heladera, Colaborador colaborador, TipoSuscripcion tipo) {
         this.heladera = heladera;
         this.colaborador = colaborador;
         this.tipo = tipo;
     }
+
+    public void notificarmeAlerta()
+
 }
