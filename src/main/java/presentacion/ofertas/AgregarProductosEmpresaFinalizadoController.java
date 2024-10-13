3477039c6615bca509f1c0516e5f81c524c7c9e8
Giansantucho <gsantucho@frba.utn.edu.ar>
2024-10-13 20:00:43 -0300

    holiwis

--- src/main/java/presentacion/ofertas/AgregarProductosEmpresaFinalizadoController.java
+++ src/main/java/presentacion/ofertas/AgregarProductosEmpresaFinalizadoController.java
@@ -2,23 +2,29 @@ package presentacion.ofertas;
 
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import io.javalin.http.UploadedFile;
+import modelo.colaboracion.Oferta;
 import org.jetbrains.annotations.NotNull;
 import persistencia.RepositorioIncidentes;
 import persistencia.RepositorioOfertas;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class AgregarProductosEmpresaFinalizadoController implements Handler {
 
     private RepositorioOfertas repoOfertas;
 
-    public AgregarProductosEmpresaFinalizadoController(RepositorioOfertas reposiOfertas) {
-        super();
-        this.repoOfertas = reposiOfertas;
+    public AgregarProductosEmpresaFinalizadoController(RepositorioOfertas repoOfertas) {
+        this.repoOfertas = repoOfertas;
     }
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
+
         String nombre = Objects.requireNonNull(context.formParam("nombre"));
         String tipo = Objects.requireNonNull(context.formParam("tipo"));
         String descripcion = Objects.requireNonNull(context.formParam("descripcion"));
@@ -26,5 +32,5 @@ public class AgregarProductosEmpresaFinalizadoController implements Handler {
 
         context.redirect("/aceptarAgregarProducto");
     }
-
 }
+
