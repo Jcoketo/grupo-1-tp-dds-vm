570863303b28cbfb9b148a62dcbddb2cbbc4c912
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-10-12 21:08:57 -0300

    erores

--- src/main/java/presentacion/ReportarFallaTecnicaController.java
+++ src/main/java/presentacion/ReportarFallaTecnicaController.java
@@ -3,11 +3,16 @@ package presentacion;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
+import persistencia.RepositorioIncidentes;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReportarFallaTecnicaController implements Handler {
+
+    public ReportarFallaTecnicaController() {
+        super();
+    }
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
