6de0568ec6fb1de9df254de56b1434d4b604c238
adriangilt02 <agiltolentino@frba.utn.edu.ar>
2024-10-13 23:36:25 -0300

    Distribucion de viandas + mapa eleccion

--- src/main/java/presentacion/colaboraciones/DonarDistribucionViandaController.java
+++ src/main/java/presentacion/colaboraciones/DonarDistribucionViandaController.java
@@ -15,8 +15,30 @@ public class DonarDistribucionViandaController implements Handler {
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
-        Map<String, Object> model = new HashMap<>();
-        context.render("templates/donarDistribuirViandas.mustache", model);
+        Map<String, Object> modelo = new HashMap<>();
+
+        String heladera1 = context.queryParam("heladera1");
+        String direccion1 = context.queryParam("direccion1");
+        String estado1 = context.queryParam("estado1");
+        String disponibilidad1 = context.queryParam("disponibilidad1");
+
+        String heladera2 = context.queryParam("heladera2");
+        String direccion2 = context.queryParam("direccion2");
+        String estado2 = context.queryParam("estado2");
+        String disponibilidad2 = context.queryParam("disponibilidad2");
+
+        modelo.put("heladera1", heladera1);
+        modelo.put("direccion1", direccion1);
+        modelo.put("estado1", estado1);
+        modelo.put("disponibilidad1", disponibilidad1);
+
+        modelo.put("heladera2", heladera2);
+        modelo.put("direccion2", direccion2);
+        modelo.put("estado2", estado2);
+        modelo.put("disponibilidad2", disponibilidad2);
+
+
+        context.render("templates/donarDistribuirViandas.mustache", modelo);
 
     }
 }
