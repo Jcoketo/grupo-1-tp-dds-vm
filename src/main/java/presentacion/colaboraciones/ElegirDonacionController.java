e28fb25204cc9bb346feb3a23a0305d7e83c79aa
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-10-20 14:59:21 -0300

    ??

--- src/main/java/presentacion/colaboraciones/ElegirDonacionController.java
+++ src/main/java/presentacion/colaboraciones/ElegirDonacionController.java
@@ -5,6 +5,8 @@ import io.javalin.http.Handler;
 import modelo.personas.TipoPersona;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class ElegirDonacionController implements Handler {
@@ -14,11 +16,13 @@ public class ElegirDonacionController implements Handler {
     public void handle(@NotNull Context context) throws Exception {
 
         if (Objects.equals(context.sessionAttribute("logueado"), true)) {
+            Map<String, Object> model = new HashMap<>();
             TipoPersona tipoPersona = context.sessionAttribute("tipoPersona");
+            //model.put("tipoPer", tipoPersona);
             if (tipoPersona == TipoPersona.PJ) {
-                context.redirect("/elegirDonacionJuridica");
+                context.render("templates/elegirDonacionJuridica.mustache",model);
             } else {
-                context.redirect("/elegirDonacionFisica");
+                context.render("templates/elegirDonacionFisica.mustache",model);
             }
         } else {
             context.redirect("/login");
