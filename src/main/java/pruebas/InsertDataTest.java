d4e17f264bd67f32f088b5ce11e916d03273e7ae
adriangilt02 <agiltolentino@frba.utn.edu.ar>
2024-10-31 13:49:13 -0300

    Solucion de entidades con error

--- src/main/java/pruebas/InsertDataTest.java
+++ src/main/java/pruebas/InsertDataTest.java
@@ -32,25 +32,30 @@ public class InsertDataTest {
 
         // Crear y persistir una PersonaJuridica
         MedioDeContacto medioDeContactoPJ = new MedioDeContacto(TipoMedioDeContacto.TELEFONO, "123456789");
+        em.persist(medioDeContactoPJ);  // Persistir el medio de contacto antes de PersonaJuridica
+
         PersonaJuridica personaJuridica = new PersonaJuridica("Empresa S.A.", TipoJuridica.GUBERNAMENTAL, Rubro.ELECTRONICA, medioDeContactoPJ);
         em.persist(personaJuridica);
 
-
         PuntoEstrategico puntoEstrategico = new PuntoEstrategico(13.0, 13.0);
+        em.persist(puntoEstrategico);  // Persistir PuntoEstrategico antes de asignarlo a Heladera
 
-        Heladera heladera = new Heladera(14, LocalDate.now(),puntoEstrategico);
-
+        Heladera heladera = new Heladera(14, LocalDate.now(), puntoEstrategico);
         em.persist(heladera);
 
         Colaborador colaborador = new Colaborador(personaJuridica);
+        em.persist(colaborador);  // Persistir Colaborador antes de asignarlo a SuscriptoCantidad
 
         SuscriptoCantidad suscriptoCantidad = new SuscriptoCantidad(heladera, colaborador, TipoSuscripcion.QUEDAN_POCAS, 10, TipoMedioDeContacto.MAIL);
-
-        personaJuridica.getHeladeras().add(heladera);
-
         em.persist(suscriptoCantidad);
 
+        // Establecer relaciones después de persistir
+        personaJuridica.getHeladeras().add(heladera);
         heladera.getColaboradoresSucriptos().add(suscriptoCantidad);
+
+
+
+
 
 
         /*
