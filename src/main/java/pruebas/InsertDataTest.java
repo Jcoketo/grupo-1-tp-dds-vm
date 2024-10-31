d77c936ec7477f0a21db1d5653537dfff449ac15
adriangilt02 <agiltolentino@frba.utn.edu.ar>
2024-10-31 00:56:09 -0300

    Agrego las relaciones entre heladera y  PJ

--- src/main/java/pruebas/InsertDataTest.java
+++ src/main/java/pruebas/InsertDataTest.java
@@ -1,9 +1,14 @@
 package pruebas;
 
+import modelo.elementos.Heladera;
+import modelo.elementos.PuntoEstrategico;
 import modelo.personas.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class InsertDataTest {
     public static void main(String[] args) {
@@ -14,16 +19,38 @@ public class InsertDataTest {
 
         em.getTransaction().begin();
 
+        /*
         // Crear y persistir una PersonaHumana
         MedioDeContacto medioDeContacto = new MedioDeContacto(TipoMedioDeContacto.MAIL, "example@example.com");
         PersonaHumana personaHumana = new PersonaHumana("John", "Doe", medioDeContacto);
         em.persist(personaHumana);
-/*
+        */
+
+
         // Crear y persistir una PersonaJuridica
         MedioDeContacto medioDeContactoPJ = new MedioDeContacto(TipoMedioDeContacto.TELEFONO, "123456789");
         PersonaJuridica personaJuridica = new PersonaJuridica("Empresa S.A.", TipoJuridica.GUBERNAMENTAL, Rubro.ELECTRONICA, medioDeContactoPJ);
         em.persist(personaJuridica);
-*/
+
+
+        PuntoEstrategico puntoEstrategico = new PuntoEstrategico(13.0, 13.0);
+
+        Heladera heladera = new Heladera(14, LocalDate.now(),puntoEstrategico);
+
+        em.persist(heladera);
+
+        personaJuridica.getHeladeras().add(heladera);
+
+
+        /*
+        List<Heladera> heladeras = em
+                // equivalente a: select * from persona where persona.nombre = 'Julian'
+                .createQuery("select h from Heladera h where h.id = 3" , Heladera.class) //ojo, query no tipada
+                .getResultList();
+
+        personaJuridica.setHeladeras(heladeras);
+        */
+
         em.getTransaction().commit();
         em.close();
         emf.close();
