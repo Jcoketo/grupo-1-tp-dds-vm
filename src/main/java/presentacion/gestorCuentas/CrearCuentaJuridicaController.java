74d548f2d5172a9358d3a2f0f18a1a534200da6f
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-12-04 21:02:51 -0300

    algo mas

--- src/main/java/presentacion/gestorCuentas/CrearCuentaJuridicaController.java
+++ src/main/java/presentacion/gestorCuentas/CrearCuentaJuridicaController.java
@@ -11,12 +11,12 @@ public class CrearCuentaJuridicaController implements Handler {
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
-        Map<String, Object> model = new HashMap<>();
+        Map<String, Object> model = context.sessionAttribute("model");
+        if (model == null) {
+            model = new HashMap<>();
+            context.sessionAttribute("model", model);
+        }
         context.render("templates/crearCuentaJuridica.mustache", model);
-
-
-        // aca hay que ir a dar de alta e usuario en la base de datos
-
 
     }
 }
