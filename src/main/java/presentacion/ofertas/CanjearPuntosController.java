4f2ab0013177ffb26287753bf0186a56ab88d14f
adriangilt <agiltolentino@frba.utn.edu.ar>
2024-12-11 23:58:13 -0300

    Agrego filtro en canjear puntos, falta vista ver ofertas canjeadas

--- src/main/java/presentacion/ofertas/CanjearPuntosController.java
+++ src/main/java/presentacion/ofertas/CanjearPuntosController.java
@@ -3,6 +3,7 @@ package presentacion.ofertas;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import modelo.colaboracion.Oferta;
+import modelo.colaboracion.TipoOferta;
 import modelo.personas.Colaborador;
 import modelo.personas.TipoPersona;
 import org.jetbrains.annotations.NotNull;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CanjearPuntosController implements Handler {
 
@@ -40,7 +42,34 @@ public class CanjearPuntosController implements Handler {
         TipoPersona tipoPer = context.sessionAttribute("tipoPersona");
         Colaborador colab = repoColaboradores.buscarColaboradorXIdPersona(idPersona);
 
+        String tipoOferta = context.queryParam("tipoOferta");
         List<Oferta> ofertas = repoOfertas.conocerOfertasDisponibles();
+
+        TipoPersona tipoPersona = context.sessionAttribute("tipoPersona");
+
+        Boolean esPersonaJuridica = tipoPersona.equals(TipoPersona.PJ);
+
+        model.put("esPersonaJuridica",esPersonaJuridica);
+
+        if(tipoOferta == null){
+            model.put("puntos", colab.getPuntaje());
+            model.put("ofertas", ofertas);
+
+            context.render("templates/canjearPuntos.mustache",model);
+            return;
+        }
+
+        switch (tipoOferta) {
+            case "producto":
+                ofertas = ofertas.stream().filter(oferta -> oferta.getTipoOferta() == TipoOferta.PRODUCTO).collect(Collectors.toList());
+                break;
+            case "servicio":
+                ofertas = ofertas.stream().filter(oferta -> oferta.getTipoOferta() == TipoOferta.SERVICIO).collect(Collectors.toList());
+                break;
+            default:
+                break;
+        }
+
 
         model.put("puntos", colab.getPuntaje());
         model.put("ofertas", ofertas);
