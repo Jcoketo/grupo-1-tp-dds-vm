698a92875b2c48da03b411b3bb665e3913450bf7
adriangilt <agiltolentino@frba.utn.edu.ar>
2024-12-14 17:01:50 -0300

    SSO

--- src/main/java/modelo/authService/AuthServiceUsuario.java
+++ src/main/java/modelo/authService/AuthServiceUsuario.java
@@ -37,8 +37,6 @@ public class AuthServiceUsuario {
             throw new ExcepcionValidacion("El usuario no existe");
         }
 
-
-
         String passwordBase = repoUsuarios.traerClavexUsuario(mail);
         return checkPassword(password, passwordBase);
     }
