7d1d8136638a466f7e784be94db218ae130be3f8
Jcoketo <jmenazzibaldini@frba.utn.edu.ar>
2024-12-14 21:05:14 -0300

    algunas correcciones

--- src/main/java/presentacion/gestorCuentas/CrearCuentaFisicaController.java
+++ src/main/java/presentacion/gestorCuentas/CrearCuentaFisicaController.java
@@ -3,6 +3,7 @@ package presentacion.gestorCuentas;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
+import utils.GeneradorModel;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,10 +12,8 @@ public class CrearCuentaFisicaController implements Handler {
 
     @Override
     public void handle(@NotNull Context context) throws Exception {
-        Map<String, Object> model = context.sessionAttribute("model");
-        if (model == null) {
-            model = new HashMap<>();
-        }
+        Map<String, Object> model = GeneradorModel.getModel(context);
+
         context.render("templates/crearCuentaFisica.mustache", model);
 //        context.sessionAttribute("model", null);
 
